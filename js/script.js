9a0937c895dc1a8ddf3db970c44367c25ef098ad
Andrew Helander <helander@live.com>
2021-02-23 15:46:57 -0800

    began coding covid resource page

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 var navBar = document.getElementById("collapsableNav");
 var navToggler = document.getElementById("togglerButton");
 var pageTitle = "Home";
+document.title = pageTitle
 var overlay = document.getElementById("overlay");
 var screenHeight = window.innerHeight;
-
 var calendarUrl = [
 				"https://nebula.wsimg.com/518cd4c2d9ee267321dcc5dfb1792e4c?AccessKeyId=6A70C08022DEA068CA48&disposition=0&alloworigin=1", 
 				"https://nebula.wsimg.com/a02a54c6c369bc19837afb7b379b4000?AccessKeyId=6A70C08022DEA068CA48&disposition=0&alloworigin=1",
@@ -12,7 +12,8 @@ var calendarUrl = [
 				"https://nebula.wsimg.com/b61f0e9acbf9d0bfe9eef9ead4854431?AccessKeyId=6A70C08022DEA068CA48&disposition=0&alloworigin=1"
 				]
 
-var board = [{name:"Connie Verhulst", occupation:"president", fta_leadership: true},
+var board = [
+			 {name:"Connie Verhulst", occupation:"president", fta_leadership: true},
 			 {name:"Robert Basset", occupation:"vice president", fta_leadership: true},
 			 {name:"Robb Lash", occupation:"secretary", fta_leadership: true},
 			 {name:"Troy Liggins", occupation:"treasurer", fta_leadership: true},
@@ -25,9 +26,83 @@ var board = [{name:"Connie Verhulst", occupation:"president", fta_leadership: tr
 			 {name:"Glennon Poirier", occupation:"high school director", fta_leadership: false},
 			 {name:"Mark Schulte", occupation:"high school director", fta_leadership: false}]
 
+var snippet = [
+			 {url:"snippets/home-snippet.html", id:"#homeLink", title:"Home"},
+			 {url:"snippets/about-snippet.html", id:"#aboutUsLink", title:"About Us"},
+			 {url:"snippets/calendar-snippet.html", id:"#calendarLink", title:"Calendars"},
+			 {url:"snippets/faq-snippet.html", id:"#faqLink", title:"F.A.Q.'s"},
+			 {url:"snippets/covid-snippet.html", id:null, title:"Covid-19 Resources"}
+]
+
+$.get(snippet[0].url, function(data) {
+	 $("#mainContent").html(data); 
+	});
+	$(snippet[0].id).addClass("active");
+
+function openNav () {
+var screenWidth = window.innerWidth;
+overlay.style.display = "block";
+if (screenWidth > 566) 	{
+	navBar.style.width = "300px";
+
+}
+		else {
+			navBar.style.width = "175px";
+		}
+}
+
+function closeNav () {
+	navBar.style.width = "0px";
+	overlay.style.display = "none";
+}
+
+window.addEventListener("resize", function (){
+	if (window.innerWidth > 992){
+		overlay.style.display = "none"
+		navBar.style.width = "0px"
+	}
+})
+overlay.addEventListener("click", closeNav);
+window.addEventListener("resize", function (){
+	if (window.innerWidth < 992) {
+		var calendar = document.getElementById("calendar-box");
+			$(calendar).html(" ");
+		$('.calendar-nav').removeClass('active')
+
+	}
+})
+/*/////////////////////////////////////////////////////////*/
+/*/////FUNCTIONS FOR DYNAMICALLY LOADING CONTENT///////////*/
+/*/////////////////////////////////////////////////////////*/
+
+var buildNewPage = function(array) {
+	closeNav();
+	document.title = snippet[array].title;
+	var pageId = snippet[array].id;
+	var html = snippet[array].url;
+	$(".nav-link").removeClass('active');
+	$(".smallFooterLink>a").removeClass('active');
+$(pageId).addClass('active');
+$.get(html, function(data) {
+  $("#mainContent").html(data);
+});}
+
+function buildCalendar(heading, array) {
+	var calendar = document.getElementById("calendar-box");
+	console.log (calendar)
+	var html =  "<div class='col-xs-12 col-sm-10 auto-margin'>" + "<h1 class='text-center heading-no-shadow'>" + heading + "</h1>";
+		html += "<div class='text-center externalLink'>" + "<a href=" + calendarUrl[array] + " target='_blank'>";
+		html += "Click Here to Open in New Page" + "</a></div>" + "<iframe src=" + calendarUrl[array];
+		html += "frameborder = '0 '" + "width='100%' height='650px'" + "</iframe></div>";
+	$(calendar).html(html);
+	$('.calendar-nav').removeClass('active');
+	var activeLink = '.calendar-nav' + ':nth-child(' + (array + 1) + ")";
+	$(activeLink).addClass('active')
+}
+
 var buildBoardPage = function () {
-	var html = "<div class='row'>" + "<div class='col-xs-12 col-sm-12 heading'>";
-		html += "<h1 class='text-center'>" + "Executive Board" + "</h1></div>"
+	var html = "<div class='row'>" + "<div class='col-xs-12 col-sm-12'>";
+		html += "<h1 class='text-center heading'>" + "Executive Board" + "</h1></div>"
 	var page = document.getElementById("mainContent");
 	for (i=0; i < board.length; i++) {
 		html += "<div class = 'col-xs-12 col-sm-6 contentBox'>" + "<div class='content text-center'>"
@@ -47,64 +122,3 @@ var buildBoardPage = function () {
 
 }
 
-function openNav () {
-var screenWidth = window.innerWidth;
-overlay.style.display = "block";
-if (screenWidth > 566) 	{
-	navBar.style.width = "300px";
-
-}
-		else {
-			navBar.style.width = "175px";
-		}
-}
-
-function closeNav () {
-	navBar.style.width = "0px";
-	overlay.style.display = "none";
-}
-
-document.title = pageTitle
-	$.get('snippets/home-snippet.html', function(data) {
-	  $("#mainContent").html(data); 
-	});
-	$("#homeLink").addClass("active");
-
-window.addEventListener("resize", function (){
-	if (window.innerWidth > 992){
-		overlay.style.display = "none"
-		navBar.style.width = "0px"
-	}
-})
-overlay.addEventListener("click", closeNav);
-window.addEventListener("resize", function (){
-	if (window.innerWidth < 992) {
-		var calendar = document.getElementById("calendar-box");
-			$(calendar).html(" ");
-		$('.calendar-nav').removeClass('active')
-
-	}
-})
-
-function buildPage (snippetHtml, pageTitle, pageId) {
-	closeNav();
-	document.title = pageTitle;
-	$(".nav-link").removeClass('active');
-	$(".smallFooterLink>a").removeClass('active');
-	$(pageId).addClass('active');
-$.get(snippetHtml, function( data ) {
-  $("#mainContent").html(data);
-});}
-
-function buildCalendar(heading, array) {
-	var calendar = document.getElementById("calendar-box");
-	console.log (calendar)
-	var html =  "<div class='col-xs-12 col-sm-10'>" + "<h1 class='text-center heading'>" + heading + "</h1>";
-		html += "<div class='text-center externalLink'>" + "<a href=" + calendarUrl[array] + " target='_blank'>";
-		html += "Click Here to Open in New Page" + "</a></div>" + "<iframe src=" + calendarUrl[array];
-		html += "frameborder = '0 '" + "width='100%' height='650px'" + "</iframe></div>";
-	$(calendar).html(html);
-	$('.calendar-nav').removeClass('active');
-	var activeLink = '.calendar-nav' + ':nth-child(' + (array + 1) + ")";
-	$(activeLink).addClass('active')
-}
