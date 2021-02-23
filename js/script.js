7caa5b3d410d5e5de7a0e103e6a7f471f50907b1
Andrew Helander <helander@live.com>
2021-02-22 17:33:17 -0800

    started coding calendar page

--- js/script.js
+++ js/script.js
@@ -4,7 +4,26 @@ var pageTitle = "Home"
 var overlay = document.getElementById("overlay")
 var screenHeight = window.innerHeight
 
+var calendarUrl = [
+				"http://nebula.wsimg.com/518cd4c2d9ee267321dcc5dfb1792e4c?AccessKeyId=6A70C08022DEA068CA48&disposition=0&alloworigin=1", 
+				"http://nebula.wsimg.com/a02a54c6c369bc19837afb7b379b4000?AccessKeyId=6A70C08022DEA068CA48&disposition=0&alloworigin=1",
+				"http://nebula.wsimg.com/3d7c983534c782a82991b2ff70ac8d86?AccessKeyId=6A70C08022DEA068CA48&disposition=0&alloworigin=1",
+				"http://nebula.wsimg.com/930b0761fea79f6e4647875b5041d1d0?AccessKeyId=6A70C08022DEA068CA48&disposition=0&alloworigin=1",
+				"http://nebula.wsimg.com/b61f0e9acbf9d0bfe9eef9ead4854431?AccessKeyId=6A70C08022DEA068CA48&disposition=0&alloworigin=1"
+				]
 
+var board = [{name:"Connie Verhulst", occupation:"president", fta_leadership: true},
+			 {name:"Robert Basset", occupation:"vice president", fta_leadership: true},
+			 {name:"Robb Lash", occupation:"secretary", fta_leadership: true},
+			 {name:"Troy Liggins", occupation:"treasurer", fta_leadership: true},
+			 {name:"Deborah Garland", occupation:"elementary director", fta_leadership: false},
+			 {name:"Margie Brynda", occupation:"elementary director", fta_leadership: false},
+			 {name:"Collen Gerke", occupation:"elementary director", fta_leadership: false},
+			 {name:"Barbi Maddox", occupation:"elementary director", fta_leadership: false},
+			 {name:"Amy Aydin", occupation:"middle school director", fta_leadership: false},
+			 {name:"Jodi Dominguez", occupation:"middle school director", fta_leadership: false},
+			 {name:"Glennon Poirier", occupation:"high school director", fta_leadership: false},
+			 {name:"Mark Schulte", occupation:"high school director", fta_leadership: false}]
 
 var buildBoardPage = function () {
 	var html = "<div class='row'>" + "<div class='col-xs-12 col-sm-12 heading'>";
@@ -17,50 +36,17 @@ var buildBoardPage = function () {
 		html += board[i].occupation + "<br />";
 			if (board[i].fta_leadership) {
 				html += "FTA Leadership" + "<br />";
-
-			}
-		
+			}	
 		html += "</span>" + "</div>" + "</div>";
 	}
-	html += "</div>" ;
-	$(page).html(html);
-	$(".nav-link").removeClass("active");
-	$("#execBoardLink").addClass("active");
-	closeNav();
+		html += "</div>" ;
+		$(page).html(html);
+		$(".nav-link").removeClass("active");
+		$("#execBoardLink").addClass("active");
+		closeNav();
+		document.title = "Executive Board";
 
 }
-
-window.addEventListener("resize", function (){
-	if (document.documentElement.clientWidth > 992){
-		overlay.style.display = "none"
-		navBar.style.width = "0px"
-	}
-})
-
-function getRequestObject() {
-	if (global.XMLHttpRequest) {
-		return (new XMLHttpRequest());
-	};
-}
-
-var sendGetRequest =  function(requestUrl, responseHandler, isJsonResponse) {
-    var request = getRequestObject();
-    request.onreadystatechange = 
-      function() { 
-        handleResponse(request, 
-                       responseHandler,
-                       isJsonResponse); 
-      };
-    request.open("GET", requestUrl, true);}
-
-document.title = pageTitle
-
-$.get('snippets/home-snippet.html', function(data) {
-  $("#mainContent").html(data);
-  
-});
-
-$("#homeLink").addClass("active");
 
 function openNav () {
 var screenWidth = window.innerWidth;
@@ -79,6 +65,20 @@ function closeNav () {
 	overlay.style.display = "none";
 }
 
+document.title = pageTitle
+	$.get('snippets/home-snippet.html', function(data) {
+	  $("#mainContent").html(data); 
+	});
+	$("#homeLink").addClass("active");
+
+window.addEventListener("resize", function (){
+	if (document.documentElement.clientWidth > 992){
+		overlay.style.display = "none"
+		navBar.style.width = "0px"
+	}
+})
+overlay.addEventListener("click", closeNav);
+
 function buildPage (snippetHtml, pageTitle, pageId) {
 	closeNav();
 	document.title = pageTitle;
@@ -86,22 +86,14 @@ function buildPage (snippetHtml, pageTitle, pageId) {
 	$(".smallFooterLink>a").removeClass('active');
 	$(pageId).addClass('active');
 $.get(snippetHtml, function( data ) {
-  $( "#mainContent" ).html( data );
+  $("#mainContent").html(data);
 });}
 
-overlay.addEventListener("click", closeNav);
-
-var navHeading = document.getElementById("fta-full");
-
-var board = [{name:"Connie Verhulst", occupation:"president", fta_leadership: true},
-{name:"Robert Basset", occupation:"vice president", fta_leadership: true},
-{name:"Robb Lash", occupation:"secretary", "fta_leadership": true},
-{name:"Troy Liggins", occupation:"treasurer", "fta_leadership": true},
-{name:"Deborah Garland", occupation:"elementary director", "fta_leadership": false},
-{name:"Margie Brynda", occupation:"elementary director", "fta_leadership": false},
-{name:"Collen Gerke", occupation:"elementary director", "fta_leadership": false},
-{name:"Barbi Maddox", occupation:"elementary director", "fta_leadership": false},
-{name:"Amy Aydin", occupation:"middle school director", "fta_leadership": false},
-{name:"Jodi Dominguez", occupation:"middle school director", "fta_leadership": false},
-{name:"Glennon Poirier", occupation:"high school director", "fta_leadership": false},
-{name:"Mark Schulte", occupation:"high school director","fta_leadership": false}]
+function buildCalendar(heading, array) {
+	var calendar = document.getElementById('calendar-box');
+	var html =  "<div class='col-xs-12 col-sm-10'>" + "<h1 class='text-center heading'>" + heading + "</h1>";
+		html += "<div class='text-center externalLink'>" + "<a href=" + calendarUrl[array] + " target='_blank'>";
+		html += "Click Here to Open in New Page" + "</a></div>" + "<iframe src=" + calendarUrl[array];
+		html += "frameborder = '0 '" + "width='100%' height='650px'" + "</iframe></div>";
+	$(calendar).html(html);
+}
